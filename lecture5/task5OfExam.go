68dfce2baa86c61b42cee869a02a61eb4dae08d4
Siyovush <paraparadox@protonmail.com>
2023-03-06 20:49:26 +0500

    Exam task upgrade

--- lecture5/task5OfExam.go
+++ lecture5/task5OfExam.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -15,13 +16,55 @@ func RandStringBytes(n int) string {
 	return string(b)
 }
 
+type Employee struct {
+	ID            int
+	CompanyID     int
+	Name          string
+	Surname       string
+	Email         string
+	DateOfJoining time.Time
+	Position      string
+	Salary        int
+}
+
+func (e *Employee) TotalMoneyEarned() int {
+	totalMonths := time.Now().Sub(e.DateOfJoining).Hours() / 24 / 30
+	return int(totalMonths) * e.Salary
+}
+
+func NewEmployee(companyID int) Employee {
+	var employee Employee
+
+	employee.ID = rand.Intn(2000) + 1
+	employee.CompanyID = companyID
+	employee.Name = RandStringBytes(5)
+	employee.Surname = RandStringBytes(15)
+	employee.Email = RandStringBytes(10)
+
+	years := rand.Intn(5)
+	months := rand.Intn(12)
+
+	employee.DateOfJoining = time.Now().AddDate(-years, -months, 0)
+	employee.Position = RandStringBytes(10)
+	employee.Salary = rand.Intn(6000) + 1 + 1000
+
+	return employee
+}
+
 type Company struct {
+	ID                 int
 	Name               string
 	Location           string
 	YearOfFoundation   int
 	CurrentYearIncome  int
 	PreviousYearIncome int
-	Employees          []string
+	Employees          []Employee
+}
+
+// TotalMoneySpentOnSalaries возвращает общее количество денег потраченных на оплату труда сотрудников
+func (c *Company) TotalMoneySpentOnSalaries() (totalMoney int) {
+
+	return
 }
 
 // YearsOfExistence возвращает разницу между текущим годом и годом основания компании
@@ -37,7 +80,7 @@ func (c *Company) IncomeDifference() int {
 // HasEmployee принимает имя сотрудника и проверяет есть ли такой сотрудник в массиве сотрудников компании
 func (c *Company) HasEmployee(name string) bool {
 	for _, employee := range c.Employees {
-		if employee == name {
+		if employee.Name == name {
 			return true
 		}
 	}
@@ -47,16 +90,22 @@ func (c *Company) HasEmployee(name string) bool {
 // implement employee
 
 func (c *Company) PrintInfo() {
+	fmt.Printf("ID:\t\t\t%d\n", c.ID)
 	fmt.Printf("Name:\t\t\t%s\n", c.Name)
 	fmt.Printf("Location:\t\t%s\n", c.Location)
 	fmt.Printf("Year of foundation:\t%d\n", c.YearOfFoundation)
 	fmt.Printf("Current year income:\t%d\n", c.CurrentYearIncome)
 	fmt.Printf("Previous year income:\t%d\n", c.PreviousYearIncome)
+	fmt.Println("Employees")
+	for _, e := range c.Employees {
+		fmt.Println(e)
+	}
 }
 
 func NewCompany() Company {
 	var company Company
 
+	company.ID = rand.Intn(60) + 1
 	company.Name = RandStringBytes(5)
 	company.Location = RandStringBytes(10)
 	company.YearOfFoundation = rand.Intn(1000) + 1000
@@ -65,9 +114,9 @@ func NewCompany() Company {
 
 	employeesNumber := rand.Intn(50) + 1
 
-	company.Employees = make([]string, employeesNumber)
+	company.Employees = make([]Employee, employeesNumber)
 	for i := 0; i < employeesNumber; i++ {
-		company.Employees[i] = RandStringBytes(15)
+		company.Employees[i] = NewEmployee(company.ID)
 	}
 	return company
 }
