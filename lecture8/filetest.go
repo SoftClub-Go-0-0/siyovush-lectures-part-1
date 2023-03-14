507f684f74c26ad745c5e19b3611ccd4afde7886
Siyovush <paraparadox@protonmail.com>
2023-03-14 19:42:23 +0500

    Reading integers modified

--- lecture8/filetest.go
+++ lecture8/filetest.go
@@ -10,14 +10,17 @@ import (
 func main() {
 	var filename string
 
-	fmt.Scan(&filename)
+	_, err := fmt.Scan(&filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanWords)
@@ -33,5 +36,21 @@ func main() {
 		numbers = append(numbers, number)
 	}
 
+	file.Close()
+
+	fmt.Printf("%v\t%T\n", file, file)
 	fmt.Println(numbers)
+
+	file, err = os.OpenFile(filename, os.O_WRONLY|os.O_APPEND, 0666)
+	for _, number := range numbers {
+		_, err := file.WriteString(strconv.Itoa(number*2) + " ")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	file.Close()
+
+	fmt.Println("All done!")
+
 }
